15fdd6275f4a7f6f959b5b6c794d7a458d9c69ea
UnInformaticoAburrido <dimitrycompanycifre@gmail.com>
2024-03-21 19:33:59 +0100

    Casi fianlizado la barra de menus

---
+++ PracticaFinal/src/practicaFinal/Opciones.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package practicaFinal;
+
+/**
+ *
+ * @author dimitry
+ * Esta clase tiene como objetivo quitarnos trabajo centraliznado las funcionalidades
+ */
+public class Opciones {
+    public static void partida(){
+            //Esta funcion deve iniciar la partida
+            System.out.println("Funcionalidad no implementada");
+        }
+        public static void configuracion(){
+            //Esta funcion deve  habrir la configuracion
+            System.out.println("Funcionalidad no implementada");
+        }
+        public static void historial(){
+            //Esta funcion deve habrir el historial
+            System.out.println("Funcionalidad no implementada");
+        }
+        public static void informacion(){
+            //Esta funcion deve mostrar la informacion
+            System.out.println("Funcionalidad no implementada");
+        }
+        public static void salir(){
+            //Esta funcion deve finalizar el progrma
+            System.out.println("Funcionalidad no implementada");
+        }
+}
