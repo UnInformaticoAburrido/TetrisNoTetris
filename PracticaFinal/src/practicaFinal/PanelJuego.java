b854c2428376c514d3d609b6e30b049a0e07301f
Jenrikku <60291632+Jenrikku@users.noreply.github.com>
2024-06-01 12:26:12 +0200

    Start drawing lines on game panel

--- PracticaFinal/src/practicaFinal/PanelJuego.java
+++ PracticaFinal/src/practicaFinal/PanelJuego.java
@@ -1,9 +1,35 @@
 package practicaFinal;
 
+import java.awt.*;
+
 import javax.swing.JPanel;
 
 public class PanelJuego extends JPanel {
     public PanelJuego() {
         super();
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        Graphics2D graphics2D = (Graphics2D) graphics;
+
+        graphics2D.setColor(Color.BLACK);
+
+        // Calcula dimensiones del tablero:
+        int width = 30 * 20, height = 30 * 20;
+
+        // Dibuja las líneas verticales.
+        for (int i = 0; i < 21; i++) {
+            int x = i * 30;
+
+            graphics2D.drawLine(x, 0, x, height);
+        }
+
+        // Dibuja las líneas horizontales.
+        for (int i = 0; i < 21; i++) {
+            int y = i * 30;
+
+            graphics2D.drawLine(0, y, width, y);
+        }
     }
 }
