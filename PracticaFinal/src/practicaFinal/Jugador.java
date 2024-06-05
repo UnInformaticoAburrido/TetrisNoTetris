956135fffcf65ef0aacf3d1ae3063559b417494f
UnInformaticoAburrido <dimitrycompanycifre@gmail.com>
2024-06-05 11:41:37 +0200

    Creado clase Jugador

---
+++ PracticaFinal/src/practicaFinal/Jugador.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practicaFinal;
+
+/**
+ *
+ * @author dima
+ *
+ */
+public class Jugador {
+    private String nombre;
+    private int tiempo;
+    private int puntuacion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+}
